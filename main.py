5edf36bd632083d252e35c85e20a0d8b27dae525
dimanchik79 <dimanchik.dmitri@yandex.ru>
2024-07-23 17:20:47 +0700

    add GUI authorization

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import hashlib
+import sys
+
 from DB.models import Users
+
+from PyQt5.QtWidgets import QApplication
+from authorization import Authorize
 
 
 def hash_password(password):
@@ -7,9 +12,13 @@ def hash_password(password):
 
 
 def main():
-
     password = hash_password("password")
     print(password)
+
+    application = QApplication(sys.argv)
+    main_window = Authorize()
+    main_window.show()
+    sys.exit(application.exec())
 
 
 if __name__ == "__main__":
